95ef2976adcea7cb556e97034f524af702fb5aed
Arjun Sheshadri <slendysheshadri@gmail.com>
2023-07-23 20:14:23 -0400

    first commit

--- diplomat/robot/services/snapshot_service.py
+++ diplomat/robot/services/snapshot_service.py
@@ -10,33 +10,33 @@ client = Client(transport=transport, fetch_schema_from_transport=True)
 def query_snapshot_proposal(proposal_id):
     return client.execute(gql(textwrap.dedent(f"""
         query {{
-          proposal(id:"{proposal_id}") {{
-            id
-            title
-            body
-            choices
-            start
-            end
-            snapshot
-            state
-            author
-            created
-            scores
-            scores_by_strategy
-            scores_total
-            scores_updated
-            plugins
-            network
-            strategies {{
-              name
-              network
-              params
+            proposal(id:"{proposal_id}") {{
+                id
+                title
+                body
+                choices
+                start
+                end
+                snapshot
+                state
+                author
+                created
+                scores
+                scores_by_strategy
+                scores_total
+                scores_updated
+                plugins
+                network
+                strategies {{
+                    name
+                    network
+                    params
+                }}
+                space {{
+                    id
+                    name
+                }}
             }}
-            space {{
-              id
-              name
-            }}
-          }}
         }}
     """)))
 
@@ -44,25 +44,25 @@ def query_snapshot_proposal(proposal_id):
 def query_snapshot_space(space_id):
     return client.execute(gql(textwrap.dedent(f"""
         query Spaces {{
-          spaces(where: {{id: "{space_id}"}}) {{
-            id
-            name
-            about
-            network
-            symbol
-            strategies {{
-              name
-              network
-              params
+            spaces(where: {{id: "{space_id}"}}) {{
+                id
+                name
+                about
+                network
+                symbol
+                strategies {{
+                    name
+                    network
+                    params
+                }}
+                admins
+                moderators
+                members
+                filters {{
+                    minScore
+                    onlyMembers
+                }}
+                plugins
             }}
-            admins
-            moderators
-            members
-            filters {{
-              minScore
-              onlyMembers
-            }}
-            plugins
-          }}
         }}
     """)))
